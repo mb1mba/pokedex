a1fcfb2bb1aebd37f5da135452fbf8485ad4a8ed
Mb3mba <theokimbangui36@gmail.com>
2023-08-24 16:25:35 +0200

    update not found page

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,11 +1,12 @@
 import React from "react"
 import { Link } from "react-router-dom"
 import "./style.css"
+
 export default function NotFound() {
     return (
         <div className="page home">
             <h1>Sorry, the page you were looking for was not found.</h1>
-            <Link to="/">Return to Home</Link>
+            <Link className="go-home" to="/">Return to Home</Link>
         </div>
     )
 }
